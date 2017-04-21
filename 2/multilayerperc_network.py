6e386c406ea72640228dbcfed4681712efece234
demarg <marcvolkert@gmx.de>
2017-04-21 17:31:20 +0200

    recurrent network done, only missing experiment output

--- 2/multilayerperc_network.py
+++ 2/multilayerperc_network.py
@@ -1,36 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 21 15:51:38 2017
+Created on Wed Apr 19 20:09:33 2017
 
 @author: marc
 """
 
-# LSTM for sequence classification in the IMDB dataset
+# MLP for the IMDB problem
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Flatten
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
+
 # fix random seed for reproducibility
-numpy.random.seed(7)
+seed = 7
+numpy.random.seed(seed)
+
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-# truncate and pad input sequences
-max_review_length = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+max_words = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
 # create the model
-embedding_vector_length = 32
 model = Sequential()
-model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
-model.add(LSTM(50))
+model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, epochs=3, batch_size=64)
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+
+
+
+
+
+
+
+
---
+++ 2/multilayerperc_originalplot.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 14:50:28 2017
+
+Plotting.
+
+@author: marc
+"""
+
+import numpy
+from keras.datasets import imdb
+from matplotlib import pyplot
+
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load the dataset
+(X_train, y_train), (X_test, y_test) = imdb.load_data()
+X = numpy.concatenate((X_train, X_test), axis=0)
+y = numpy.concatenate((y_train, y_test), axis=0)
+
+# summarize size
+print("Training data: ")
+print(X.shape)
+print(y.shape)
+
+# Summarize number of classes
+print("Classes: ")
+print(numpy.unique(y))
+
+# Summarize number of words
+print("Number of words: ")
+print(len(numpy.unique(numpy.hstack(X))))
+
+# Summarize review length
+print("Review length: ")
+result = [len(x) for x in X]
+print("Mean %.2f words (%f)" % (numpy.mean(result), numpy.std(result)))
+# plot review length
+pyplot.boxplot(result)
+pyplot.show()
