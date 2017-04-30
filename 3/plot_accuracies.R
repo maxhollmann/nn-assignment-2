ad91e538e9c7583040d751e0a495e4a83c68c51e
Max Hollmann <maxhollmann@gmail.com>
2017-04-30 13:49:47 +0200

    Plot accuracies over epochs to png

--- 3/plot_accuracies.R
+++ 3/plot_accuracies.R
@@ -2,13 +2,19 @@ library(ggplot2)
 
 setwd("/home/max/dev/nn/assignment2/3")
 
-d = read.csv("out/accuracy.csv")
-d$model_inst = factor(paste(d$model, d$params))
-
-for (inst in levels(d$model_inst)) {
-  di = d[d$model_inst == inst, ]
-  p = ggplot(di, aes(epoch, acc)) + theme_bw() + 
-    labs(x = "Epoch", y = "Test Accuracy", title = inst) +
-    geom_line()
-  print(p)
+for (model in list.files("out", include.dirs = T, full.names = T)) {
+  for (version in list.files(model, include.dirs = T, full.names = T)) {
+    for (acc_csv in list.files(version, "*_accuracy.csv", full.names = T)) {
+      d = read.csv(acc_csv)
+      
+      p = ggplot(d, aes(epoch, acc)) + theme_bw() + 
+        labs(x = "Epoch", y = "Test Accuracy") +
+        geom_line()
+      print(p)
+      
+      acc_png = sub("\\.csv$", ".png", acc_csv)
+      ggsave(acc_png, p)
+    }
+  }
 }
+
